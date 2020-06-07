03a84b782b29c55b6b961973cd65fbe58a16ba9a
Ilya Nikishin <ilya.ruwork@gmail.com>
2020-06-08 00:35:35 +0300

    Many chages: 1. floatimage is now a 2D array 2. Random vector directions clmped 3. Scene updated

--- src/main.cpp
+++ src/main.cpp
@@ -22,20 +22,20 @@
 // #define NO_LOG
 // #define NO_INTERMEDIATE_RESULTS
 
-const char* filename = "render.png";
+const char *filename = "render.png";
 const int saveEveryNSamples = 10;
-const int w = 401;
-const int h = 401;
-const float fov = 1.1;
+const int w = 400;
+const int h = 400;
+const float fov = 1.5;
 const float imageGamma = 1 / 2.2;
-const float sensitivity = 255 * 8;
-const int bounces = 20;
+const float sensitivity = 255;
+const int bounces = 5;
 const int spp = 1000;
 const int maxTime = 600;
-const float sampleVariation = 0.0001;
-const float farClipping = 1000;
-const float nearClipping = 0.001;
-const float specularClipping = .0001;
+const float sampleVariation = 1e-4;
+const float farClipping = 1e+6;
+const float nearClipping = 1e-6;
+const float specularClipping = 1e-6;
 
 Vec3 camPos(0, 0, 0);
 
@@ -53,19 +53,19 @@ inline float clamp(float val, float min, float max)
 // Converts a float value in range 0..1 to an int value in range 0..255
 inline unsigned char floatToChannel(float input)
 {
-    return (unsigned char) clamp(floor(pow(input, imageGamma) * sensitivity), 0, 255);
+    return (unsigned char)clamp(floor(pow(input, imageGamma) * sensitivity), 0, 255);
 }
 
-inline void _saveImage(Vec3* floatImage, png::image<png::rgb_pixel> pngImage, unsigned int sample)
+inline void _saveImage(Vec3 floatImage[][h], png::image<png::rgb_pixel> pngImage, unsigned int sample)
 {
 #pragma omp parallel for
-    for (int y = 0; y < h; y++)
+    for (int x = 0; x < w; x++)
     {
-        for (int x = 0; x < w; x++)
+        for (int y = 0; y < h; y++)
         {
-            pngImage[y][x].red = floatToChannel(floatImage[y * h + x].x / (sample - 1));
-            pngImage[y][x].green = floatToChannel(floatImage[y * h + x].y / (sample - 1));
-            pngImage[y][x].blue = floatToChannel(floatImage[y * h + x].z / (sample - 1));
+            pngImage[y][x].red = floatToChannel(floatImage[x][y].x / (sample - 1));
+            pngImage[y][x].green = floatToChannel(floatImage[x][y].y / (sample - 1));
+            pngImage[y][x].blue = floatToChannel(floatImage[x][y].z / (sample - 1));
         }
     }
     pngImage.write(filename);
@@ -79,94 +79,92 @@ inline void _saveImage(Vec3* floatImage, png::image<png::rgb_pixel> pngImage, un
 
 int main()
 {
-    Vec3* floatImage = new Vec3[w * h];
+    Vec3 floatImage[w][h];
+    for (unsigned int x = 0; x < w; x++)
+    {
+        for (unsigned int y = 0; y < h; y++)
+            floatImage[x][y] = Vec3();
+    }
     png::image<png::rgb_pixel> pngImage(w, h);
 
     int sensorSide = w > h ? w : h;
-    std::vector<Renderable*> objects;
+    std::vector<Renderable *> objects;
 
-    Material* light = new Material(
-        Vec3(1, 1, 1),
+    Material *light = new Material(
+        Vec3(3, 3, 3),
         Vec3(0, 0, 0),
         0);
 
-    Material* diffuseGrey = new Material(
+    Material *diffuseGrey = new Material(
         Vec3(0, 0, 0),
         Vec3(0.9, 0.9, 0.9),
-        0.9);
+        0.5);
 
-    Material* diffuseRed = new Material(
+    Material *diffuseRed = new Material(
         Vec3(0, 0, 0),
         Vec3(0.9, 0, 0),
         0.9);
 
-    Material* diffuseGreen = new Material(
+    Material *diffuseGreen = new Material(
         Vec3(0, 0, 0),
         Vec3(0, 0.9, 0),
         0.9);
 
-    Material* specular = new Material(
+    Material *specular = new Material(
         Vec3(0, 0, 0),
         Vec3(0.9, 0.9, 0.9),
         0.1);
 
     // Floor
     objects.push_back(new Plane(
-        Vec3(0, -1, 0),
-        Vec3(1, -1, 0),
-        Vec3(0, -1, 1),
+        Vec3(0, -0.5, 0),
+        Vec3(0, 1, 0),
         diffuseGrey));
 
     // Roof
     objects.push_back(new Plane(
-        Vec3(0, 1.5, 0),
-        Vec3(1, 1.5, 0),
-        Vec3(0, 1.5, 1),
+        Vec3(0, 2, 0),
+        Vec3(0, -1, 0),
         diffuseGrey));
 
     // Wall in front
     objects.push_back(new Plane(
         Vec3(0, 0, 5),
-        Vec3(1, 0, 5),
-        Vec3(0, 1, 5),
+        Vec3(0, 0, -1),
         diffuseGrey));
 
     // Wall in back
     objects.push_back(new Plane(
         Vec3(0, 0, -2),
-        Vec3(1, 0, -2),
-        Vec3(0, 1, -2),
+        Vec3(0, 0, 1),
         diffuseGrey));
 
     // Right wall (green)
     objects.push_back(new Plane(
         Vec3(2, 0, 0),
-        Vec3(2, 1, 0),
-        Vec3(2, 0, 1),
+        Vec3(-1, 0, 0),
         diffuseGreen));
 
     // Left wall (red)
     objects.push_back(new Plane(
         Vec3(-2, 0, 0),
-        Vec3(-2, 1, 0),
-        Vec3(-2, 0, 1),
+        Vec3(1, 0, 0),
         diffuseRed));
 
     // Light
     objects.push_back(new Disk(
-        Vec3(0, 1.49999, 3.5),
-        Vec3(1, 1.49999, 3.5),
-        Vec3(0, 1.49999, 4.5),
+        Vec3(0, 1.999, 2.5),
+        Vec3(0, -1, 0),
         1, light));
 
     // Right sphere (shiny)
     objects.push_back(new Sphere(
-        Vec3(0.75, -0.5, 3.5),
+        Vec3(0.75, 0, 2.5),
         0.5, specular));
 
     // Left sphere (diffuse)
     objects.push_back(new Sphere(
-        Vec3(-0.55, -0.4, 3.2),
+        Vec3(-0.5, 0.05, 2),
         0.6, diffuseGrey));
 
     time_t start = time(0);
@@ -181,9 +179,9 @@ int main()
     {
 #ifndef NO_LOG
         std::cout << "\r"
-                  << "Progress: " << (spp == 0 ? round((float) (allPassed + 1) / maxTime * 100) : round((float) sample / spp * 100)) << '%' << std::setw(4) << '\0'
+                  << "Progress: " << (spp == 0 ? round((float)(allPassed + 1) / maxTime * 100) : round((float)sample / spp * 100)) << '%' << std::setw(4) << '\0'
                   << "Time: " << allPassed + 1 << " sec" << std::setw(4) << '\0'
-                  << "ETA: " << (spp == 0 ? maxTime - allPassed - 1 : round((spp - sample) * ((float) allPassed / sample)))
+                  << "ETA: " << (spp == 0 ? maxTime - allPassed - 1 : round((spp - sample) * ((float)allPassed / sample)))
                   << " sec" << std::setw(4) << '\0';
         std::cout << std::flush;
 #endif
@@ -195,17 +193,18 @@ int main()
 
 // Go for every pixel in the image (IN PARALLEL)
 #pragma omp parallel for
-        for (int y = h / 2 - 1; y >= -h / 2; y--)
+        for (int x = -w / 2; x < w / 2; x++)
         {
-            for (int x = -w / 2; x < w / 2; x++)
+            for (int y = h / 2; y > -h / 2; y--)
             {
+
                 // Initial ray values
                 Vec3 result(0, 0, 0);
                 Vec3 reflection(1, 1, 1);
                 Vec3 origin = camPos;
                 Vec3 direction(
-                    (float) x / sensorSide + ((rand() % RAND_MAX) / (RAND_MAX * 0.5f) - 1) * sampleVariation,
-                    (float) y / sensorSide + ((rand() % RAND_MAX) / (RAND_MAX * 0.5f) - 1) * sampleVariation,
+                    (float)x / sensorSide + ((rand() % RAND_MAX) / (RAND_MAX * 0.5f) - 1) * sampleVariation,
+                    (float)y / sensorSide + ((rand() % RAND_MAX) / (RAND_MAX * 0.5f) - 1) * sampleVariation,
                     1 / fov);
 
                 direction = direction.normalized();
@@ -232,13 +231,6 @@ int main()
                     if (closest == -1)
                         break;
 
-                    // DEBUG
-
-                    // if (x == 0 && y == -100 && closest == 1)
-                    // {
-                    //     std::cout << "Break!\n";
-                    // }
-
                     // Save to the pixel buffer
                     result += objects.at(closest)->mat->emission * reflection;
 
@@ -247,25 +239,30 @@ int main()
 
                     // The reflection is too dark
                     if (reflection.highest() <= specularClipping)
+                    {
+                        // std::cout << "Dark object ID: " << closest << std::endl;
                         break;
+                    }
 
                     // New ray origin
-                    origin = origin + direction * lowestDistance;
+                    origin += direction * lowestDistance;
 
                     // New ray direction
                     Vec3 hitNormal = objects.at(closest)->getNormal(origin);
                     direction = direction - (hitNormal * 2) * (direction.dot(hitNormal));
-                    direction = lerp(direction,
-                        Vec3(
-                            ((float) rand() / RAND_MAX * 2 - 1),
-                            ((float) rand() / RAND_MAX * 2 - 1),
-                            ((float) rand() / RAND_MAX * 2 - 1))
-                            .normalized(),
-                        objects.at(closest)->mat->roughness);
+                    Vec3 randVec(
+                        ((float)rand() / RAND_MAX * 2 - 1),
+                        ((float)rand() / RAND_MAX * 2 - 1),
+                        ((float)rand() / RAND_MAX * 2 - 1));
+                    randVec = randVec.normalized();
+
+                    randVec = randVec.dot(hitNormal) > 0 ? randVec : -randVec;
+
+                    direction = lerp(direction, randVec, objects.at(closest)->mat->roughness);
                     direction = direction.normalized();
                 }
                 // Write the result of this sample
-                floatImage[(-y + h / 2) * h + x + w / 2] += result;
+                floatImage[w / 2 + x][h / 2 - y] += result;
             }
         }
         // Update time
@@ -287,7 +284,6 @@ int main()
     // Cleanup
     for (unsigned int i = 0; i < objects.size(); i++)
         delete objects.at(i);
-    delete[] floatImage;
     delete diffuseGrey;
     delete diffuseGreen;
     delete diffuseRed;
